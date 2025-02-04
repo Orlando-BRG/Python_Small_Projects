60b152e2b53d7f290dda406be82edf045996a4b3
Orlando-BRG <orlando.boff@gmail.com>
2025-02-04 16:17:17 -0500

    update

--- Car_Weight_Capacity.py
+++ Car_Weight_Capacity.py
@@ -1,17 +1,17 @@
 # Car weight capacity
 
-soda = "12"
-polvilho = "55"
-extra_weight = "5"
+soda = 12
+polvilho = 55
+extra_weight = 5
 
 def car_weight(soda, polvilho, extra_weight):
     try:
-        soda = float(input("Packs of soda: "))
-        polvilho = float(input("Bags of polvilho: "))
-        extra_weight = float(input("Extra items: "))
+        soda = int(input("Packs of soda: "))
+        polvilho = int(input("Bags of polvilho: "))
+        extra_weight = int(input("Extra items: "))
     except ValueError:
         print("Numbers only")
         car_weight = soda + polvilho + extra_weight
-    if car_weight > 1.300:
+    if car_weight > 1.3:
         print("Car Overload")
     print(f"total weight: {soda} + {polvilho} + {extra_weight}")
