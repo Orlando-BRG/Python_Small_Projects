25339ce7fff1c3bf57ed7bc6ab7e5ecf4bcd221a
Orlando <orlandoboff@hotmail.com>
2025-02-04 22:20:50 -0500

    update

--- Car_Weight_Capacity.py
+++ Car_Weight_Capacity.py
@@ -3,8 +3,9 @@
 # Weight of each item (in lbs)
 
 soda_weight = 12 # Each pack of soda weights 12 lbs
-polvilho_weight = 55 # Each pack of polvilho weights 55 lbs 
+polvilho_weight = 55 # Each pack of polvilho weights 55 lbs
 extra_item_weight = 5 # Each extra item weights 5 lbs
+
 
 def car_weight():
     try:
@@ -12,11 +13,13 @@ def car_weight():
         soda_count = int(input("Packs of soda: "))
         polvilho_count = int(input("Bags of polvilho: "))
         extra_item_count = int(input("Extra items: "))
-        
-        # Calculate total weight 
+
+        # Calculate total weight
     except ValueError:
         print("Numbers only")
         car_weight = (soda * polvilho * extra_weight)
     if car_weight > 1.3:
         print("Car Overload")
     print(f"total weight: {soda} + {polvilho} + {extra_weight}")
+
+#Project in progress in between machines
